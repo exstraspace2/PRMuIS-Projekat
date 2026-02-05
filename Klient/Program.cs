14008cd7cd8493e3c67ca76d68e9a987f13efd5e
exstraspace2 <djordjesopalovic@gmail.com>
2026-02-05 16:50:32 +0100

    Pravilna verzija pocetnog koda

---
+++ Klient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Klient
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+
+    }
+}
---
+++ Projekat_mreze/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_mreze
+{
+    internal class Program
+    {
+       
+    }
+}
